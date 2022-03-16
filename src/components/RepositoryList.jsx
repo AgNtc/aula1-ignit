9f7e7315c067b6e89bf64382e64cc4a7efb46e07
Agntc <aguidobispo@outlook.com>
2022-03-16 16:35:43 -0300

    first commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,21 @@
+import { RepositorioItem } from "./RepositoryItem";
+
+const repository ={
+    name: 'unform',
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform'
+}
+
+export function RepositoryList(){
+    return (
+      <section className="repository-list">
+        <h1> Lista de repositórios</h1>
+        <ul>
+          <RepositorioItem repository={repository} />
+          <RepositorioItem repository={repository} />
+          <RepositorioItem repository={repository} />
+          <RepositorioItem repository={repository} />
+        </ul>
+      </section>
+    );
+}
